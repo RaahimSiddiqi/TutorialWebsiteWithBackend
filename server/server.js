dd01dd7ed2e6ba7189209e0d3141caa4eb47671c
RaahimSiddiqi <k200146@nu.edu.pk>
2023-12-04 00:36:25 +0500

    change port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 const app = express()
 
-const PORT = 3001
+const PORT = 8080
 const distPath = path.join(__dirname, '../client/dist')
 
 app.use('/', express.static(distPath))
